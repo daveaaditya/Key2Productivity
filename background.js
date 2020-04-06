74fdb493c20ddd4ba9aa1f78e7e82ee54b19980f
Aaditya Dave <anb.dave@gmail.com>
2020-04-06 14:07:52 -0400

    Added new tab to right and new tab to left shortcuts.

--- background.js
+++ background.js
@@ -1,6 +1,18 @@
 chrome.commands.onCommand.addListener(function(command) {
     console.log('Command:', command);
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-        chrome.tabs.duplicate(tabs[0].id);
-    });
+    if (command === "duplicate_tab") {
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+            chrome.tabs.duplicate(tabs[0].id);
+        });
+    }
+    else if (command === "new_right_tab") {
+        chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+            chrome.tabs.create({index: tabs[0].index + 1});
+        });
+    }
+    else if (command === "new_left_tab") {
+        chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {        
+            chrome.tabs.create({index: tabs[0].index});
+        });
+    }
 });

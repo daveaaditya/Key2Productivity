68212feb222be7784ba435cf0f12f5f5253fcf7e
Aaditya Dave <anb.dave@gmail.com>
2020-04-09 23:25:08 -0400

    Added some more commands.

--- background.js
+++ background.js
@@ -10,9 +10,9 @@ chrome.commands.onCommand.addListener(function(command) {
             chrome.tabs.create({index: tabs[0].index + 1});
         });
     }
-    else if (command === "new_left_tab") {
-        chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {        
-            chrome.tabs.create({index: tabs[0].index});
-        });
-    }
+    // else if (command === "new_left_tab") {
+    //     chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {        
+    //         chrome.tabs.create({index: tabs[0].index});
+    //     });
+    // }
 });
